44e7b9d1a5011e085b1275259733ee78a98e511f
EveCoronel <evelyncoronel05@gmail.com>
2022-04-06 22:42:32 -0300

    Se agrega condicional IF

--- app.js
+++ app.js
@@ -18,18 +18,18 @@ function init() {
     let titulo = prompt('Comparte tu receta. Ingresa el título de tu receta')
     let contenido = prompt('Escribe el paso a paso de tu receta')
 
-    guardarReceta()
 
     console.log(recetas)
 
+    if ((titulo !== null) && (contenido !== null)) {
+        let articulo = document.getElementById('articulos')
+        let contenedor = document.createElement('div');
 
-    let contenedor = document.createElement('articule');
+        contenedor.innerHTML = `<h1> ${titulo} </h1>
+    <p> ${contenido} </p>`;
 
-    contenedor.innerHTML = `<div> 
-    <h1> ${titulo} </h1>
-    <p> ${contenido} </p>
-    </div>`;
+        articulo.appendChild(contenedor);
+        guardarReceta()
 
-    document.body.appendChild(contenedor);
-
+    }
 }
